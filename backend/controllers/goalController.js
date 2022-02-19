f52686e068a8f56d263a046d39ca5f7dc52fad8d
j.kunciene <j.kunciene@gmail.com>
2022-02-19 19:30:09 +0200

    iki user model

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,18 +1,28 @@
 const asyncHandler = require('express-async-handler');
 
+const Goal = require('../models/goalModel');
+
 // @desc Get goals
 // @route GET /api/goals
 // @access PRIVATE
-const getGoals = asyncHandler( async (req, res) => {
-    res.status(200).json({message: 'get goals'});
+const getGoals = asyncHandler( async (req, res) => {    
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 });
 
 // @desc Set goals
 // @route POST /api/goals
 // @access PRIVATE
 const setGoal = asyncHandler( async (req, res) => {
-    
-    res.status(200).json({message: 'SET  goal'});
+   
+    if(!req.body.text){
+        res.status(400)
+        throw new Error("Please add a text field");
+   }
+   const goal = await Goal.create({
+       text: req.body.text
+   })
+    res.status(200).json(goal);
 });
 
 // @desc Update goal
@@ -20,20 +30,34 @@ const setGoal = asyncHandler( async (req, res) => {
 // @access PRIVATE
 const updateGoal = asyncHandler( async (req, res) => {
 
-   if(!req.body.text){
+    const goal = await Goal.findById(req.params.id);
 
+    if(!goal){
         res.status(400)
-        throw new Error("Please add a text field");
-   }
+        throw new Error('Goal not found');
+    }
 
-    res.status(200).json({message: `Update goal ${req.params.id}`});
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+        res.status(200).json(updatedGoal);
 });
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access PRIVATE
 const deleteGoal = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`});
+
+    const goal = await Goal.findById(req.params.id);
+
+    if(!goal){
+        res.status(400);
+        throw new Error("Goal not found");
+    }
+
+    await goal.remove();
+
+    res.status(200).json({id : req.params.id});
 });
 
 module.exports = {
